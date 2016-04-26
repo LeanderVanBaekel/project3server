2a278c6335454b4510530fa44387a4be9daeda63
Wasknijper <mudkippers@hotmail.com>
2016-04-26 12:13:53 +0200

    Exported functions

---
+++ dbfunctions.js
@@ -0,0 +1,22 @@
+module.exports = {
+	insertDocument : function(db, collection, data, callback) {
+		db.collection(collection).insertOne( data, function(err, result) {
+			if (err) {console.error(err);}
+			
+			console.log("Inserted a document into the esp1 collection.");
+			callback();
+		});
+	},
+
+	findShit : function(db, collection, query, callback) {
+	   	var cursor = db.collection(collection).find(query);
+	   	cursor.each(function(err, doc) {
+			if (err) {console.error(err);}
+	    	if (doc !== null) {
+	        	console.dir(doc);
+	    	} else {
+	        	callback();
+	      	}
+	   });
+	}
+};
