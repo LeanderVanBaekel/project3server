50316514d057ff20c7d9464ce404a89f6d1c0570
Wasknijper <mudkippers@hotmail.com>
2016-04-26 14:15:55 +0200

    i herd u liek mongooses

--- routes/userInput.js
+++ routes/userInput.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var MongoClient = require('mongodb');
+var url = 'mongodb://localhost:27017/project3db';
+var dbFunctions = require('../dbfunctions.js');
+var mongoose   = require('mongoose');
+var Alarm = require('../models/alarm.js');
+mongoose.connect(url); // connect to our database
 
 
 //router.get('/', function(req, res, next){
@@ -8,19 +14,38 @@ var fs = require('fs');
 router.route('/')
 	.get(function(req, res) {
 		res.json({message : 'Trying to GET userinput'});
-	})
-	.post(function(req, res) {
-		res.json({message : 'Trying to POST userinput'});
 	});
+	// .post(function(req, res) {
+	// 	res.json({message : 'Trying to POST userinput'});
+	// });
 
 router.route('/:id')
 	.get(function(req, res) {
 		var values = req.params.id;
-		res.json({message : 'Trying to GET userinput with id: ' + req.params.id});
+		Alarm.find(function(err, alarms) {
+            if (err){
+                res.send(err);
+            }
+
+            res.json(alarms);
+        });
 	})
 	.post(function(req, res) {
-		res.json({message : 'Trying to POST userinput with id: ' + req.params.id});
-	});
+        
+        var alarm = new Alarm();      // create a new instance of the Bear model
+        alarm.id = req.params.id;  // set the bears name (comes from the request)
+        alarm.date = new Date();
+        alarm.status = null;
+
+        // save the bear and check for errors
+        alarm.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Alarm created!' });
+        });
+        
+    });
 
 
 
