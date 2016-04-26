a032a84c2529386c9800926a643e100e59a88afe
LeanderVanBaekel <leander.van.baekel@gmail.com>
2016-04-26 13:42:24 +0200

    hardware test

--- dbfunctions.js
+++ dbfunctions.js
@@ -14,6 +14,7 @@ module.exports = {
 			if (err) {console.error(err);}
 	    	if (doc !== null) {
 	        	console.dir(doc);
+	        	return doc;
 	    	} else {
 	        	callback();
 	      	}
--- routes/esp.js
+++ routes/esp.js
@@ -5,49 +5,59 @@ var MongoClient = require('mongodb');
 var url = 'mongodb://localhost:27017/project3db';
 var dbFunctions = require('../dbfunctions.js');
 
+var data;
+
+var findShit = function(db, collection, query, callback) {
+   	var cursor = db.collection(collection).find(query);
+   	cursor.each(function(err, doc) {
+		if (err) {console.error(err);}
+    	if (doc !== null) {
+        	console.dir(doc);
+        	return doc;
+    	} else {
+        	callback();
+      	}
+   });
+}
 
 
 
-router.route('/')
+router.route('/:id')
 	.get(function(req, res) {
-		res.json({message : 'Trying to GET esp data'});
-	})
-	.post(function(req, res) {
-		res.json({message : 'Trying to POST esp data'});
+		console.log("test");
+		var id = req.params.id;
+		var data = {};
+		MongoClient.connect(url, function(err, db) {
+			if (err) {console.error(err);}
+			// query id = collection name: esp1 or esp2
+			findShit(db, id, {sensor1: 'hoi'}, function() {
+				data = doc;
+				db.close();
+			});
+		});
+
+		res.json(data);
 	});
 
-router.route('/:sensor1/:sensor2/:sensor3')
-	.get(function(req, res) {
-		var sensor1 = req.params.sensor1;
-		var sensor2 = req.params.sensor2;
-		var sensor3 = req.params.sensor3;
-
-		//values = values.splice('+');
-
+router.route('/:id/:sensor1/:sensor2/:sensor3')
+	.post(function(req, res) {
+		var id = req.params.id;
 		var data = {
 			"date": new Date(),
-			"sensor1" : sensor1,
-			"sensor2" : sensor2,
-			"sensor3" : sensor3
+			"pir" : req.params.sensor1,
+			"ldr" : req.params.sensor2,
+			"motion" : req.params.sensor3
 		};
 
 		MongoClient.connect(url, function(err, db) {
-			if (err) {console.error(err);}
-			dbFunctions.insertDocument(db, 'esp1', data, function() {
+			if (err)
+				console.error(err)
+
+			dbFunctions.insertDocument(db, id, data, function() {
 				db.close();
 			});
 		});
 
-		MongoClient.connect(url, function(err, db) {
-			if (err) {console.error(err);}
-			dbFunctions.findShit(db, 'esp1', {sensor1: 'hoi'}, function() {
-				db.close();
-			});
-		});
-
-		res.json({message : 'Trying to GET esp data with id: ' + req.params.sensor1});
-	})
-	.post(function(req, res) {
 		res.json({message : 'Trying to POST esp data with id: ' + req.params.sensor1});
 	});
 
