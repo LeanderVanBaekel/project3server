dfef783b87d9a8cd91b2ce1a6a32b387041d21f7
LeanderVanBaekel <leander.van.baekel@gmail.com>
2016-04-28 12:58:11 +0200

    added alarm test

--- routes/esp.js
+++ routes/esp.js
@@ -7,37 +7,90 @@ var dbFunctions = require('../dbfunctions.js');
 var mongoose   = require('mongoose');
 var Esp1 = require('../models/esp1.js');
 var Esp2 = require('../models/esp2.js');
-
+var Alarm = require('../models/alarm.js');
 
 mongoose.createConnection(url); // connect to our database
 
-router.route('/:id')
-	.get(function(req, res) {
-		console.log("test");
-		var id = req.params.id;
 
+// TODO 
+// Crono? voor suspicion check
+// 
+
+
+// checking how often there was movement in the past 5 min
+var checkSuspicion = function(data, esp) {
+	var motion = [];
+	var motionAmount = 0;
+	for (var i = data.length - 1; i >= 0; i--) {
+		motion.push(data[i].pir);
+
+		if (data[i].pir > 0) {
+			motionAmount ++;
+		}
+	};
+	if (motionAmount > 15) {
+		ALARM(esp);
+	} 
+}
+
+// getting data to check for suspicion
+var getData = function(esp) {
+	var data;
+
+	var query = Esp1.find().sort({date: -1}).limit(30);
+	query.exec(function(err, res){
+		if (err){
+	        res.send(err);
+	    }
+	    data = res;
+	    checkSuspicion(data, esp);
+	});
+};
+
+getData("esp1");
+
+// creating new alarm 
+var ALARM = function(esp) {
+    var alarm = new Alarm();      
+    alarm.location = esp;  
+    alarm.date = new Date();
+    alarm.status = null;
+
+    alarm.save(function(err) {
+        if (err)
+            res.send(err);
+
+		console.log("Created new alarm");
+    });
+};
+
+// route to get data from the esp's
+router.route('/:id/:amount')
+	.get(function(req, res) {
+		getData("esp1");
+
+		var id = req.params.id;
+		var amount = req.params.amount;
 
 		if (id=="esp1") {
 			
-			var query = Esp1.find().sort({date: -1}).limit(10);
+			var query = Esp1.find().sort({date: -1}).limit(amount);
 
 			query.exec(function(err, data){
 				if (err){
 	                res.send(err);
 	            }
 
-	            console.log(data);
 	            res.json(data);
 			});
 		} else if (id=="esp2") {
-			var query = Esp2.find().sort({date: -1}).limit(10);
+			var query = Esp2.find().sort({date: -1}).limit(amount);
 
 			query.exec(function(err, data){
 				if (err){
 	                res.send(err);
 	            }
 
-	            console.log(data);
 	            res.json(data);
 			});
 		} else {
@@ -46,6 +99,7 @@ router.route('/:id')
 
 	});
 
+// route for the esp's to post data to
 router.route('/:id/:sensor1/:sensor2/:sensor3')
 	.post(function(req, res) {
 		var id = req.params.id;
@@ -82,8 +136,6 @@ router.route('/:id/:sensor1/:sensor2/:sensor3')
 		} else {
 			res.json({message : 'Trying to POST esp data but no esp database '});
 		};
-
-		// res.json({message : 'Trying to POST esp data with id: ' + req.params.sensor1});
 	});
 
 module.exports = router;
