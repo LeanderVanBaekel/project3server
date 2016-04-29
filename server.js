4ae9e7684771d376459b75da6d871dfff81703db
Wasknijper <mudkippers@hotmail.com>
2016-04-29 14:59:30 +0200

    index styled + mudkipz

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
+app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 8080;        // set our port
 
